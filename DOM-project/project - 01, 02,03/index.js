168c95c9ac86164eefbd20fc3f38d4e3d6823ced
pawannagthane <pawanrnagthane2566@gmail.com>
2024-08-08 14:30:14 +0530

    updated

---
+++ DOM-project/project - 01, 02,03/index.js
@@ -0,0 +1,16 @@
+ // tasl 1
+
+document.querySelector('ul li:last-child a').textContent = 'Projects';
+
+
+// task 2
+document.querySelector('.search-field input').placeholder = 'Search My Project';
+
+// task 5
+let b = document.createElement('button')
+b.textContent = 'Support me';
+document.querySelector('.hero-right-section-btns').appendChild(b)
+
+// task 4
+let i = document.querySelector('.hero-right-section img')
+i.setAttribute("src", "./0L3A3404-01.jpeg");
